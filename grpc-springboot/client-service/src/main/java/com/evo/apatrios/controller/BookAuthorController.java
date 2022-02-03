c97a24e4c6527b7e429c41d64682bd018c4a61fd
EvoMGenius <apatrios@mail.ru>
2022-02-03 13:40:21 +1000

    did collab grpc with rest

---
+++ grpc-springboot/client-service/src/main/java/com/evo/apatrios/controller/BookAuthorController.java
@@ -0,0 +1,40 @@
+package com.evo.apatrios.controller;
+
+import com.evo.apatrios.service.BookAuthorClientService;
+import com.google.protobuf.Descriptors;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/")
+public class BookAuthorController {
+
+    BookAuthorClientService bookAuthorClientService;
+
+    @GetMapping("/author/{authorId}")
+    public Map<Descriptors.FieldDescriptor, Object> getAuthor(@PathVariable String authorId){
+        return bookAuthorClientService.getAuthor(Integer.parseInt(authorId));
+    }
+
+    @GetMapping("book/{authorId}")
+    public List<Map<Descriptors.FieldDescriptor, Object>> getBooksByAuthor(@PathVariable String authorId) throws InterruptedException {
+        return bookAuthorClientService.getBooksByAuthor(Integer.parseInt(authorId));
+    }
+
+    @GetMapping("/book")
+    public Map<String,Map<Descriptors.FieldDescriptor, Object>> getExpensiveBook() throws InterruptedException {
+        return bookAuthorClientService.getExpensiveBook();
+    }
+
+    @GetMapping("book/author/{gender}")
+    public List<Map<Descriptors.FieldDescriptor, Object>> getBooksByAuthorGender(@PathVariable String gender) throws InterruptedException {
+        return bookAuthorClientService.getBooksByAuthorGender(gender);
+    }
+}
